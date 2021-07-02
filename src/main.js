3ac695fba4da8ae81f01aee3af201b901fc3efb5
Araceli Valdovinos <valdovinosaraceli50@gmail.com>
2021-07-02 12:12:40 -0500

    add input options to select desire currency

--- src/main.js
+++ src/main.js
@@ -5,16 +5,22 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import ExchangeCurrency from './exchange-currency.js';
 
+// function convertion(response) {
+
+// }
 
 function getElement(response) {
   console.log(response);
-  console.log(response['conversion_rates']);
+  console.log(response['conversion_rates']['AED']);
+  let EUR = response['conversion_rates']['EUR'];
+  console.log(5*EUR)
   // if (response['conversion_rates']) {
   //   $()
   // }
 
 }
 
+// let usDollars = 5;
 ExchangeCurrency.getCurrency()
   .then(function(response){
     getElement(response);
