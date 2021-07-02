e2c59277da8a8c5b41f4484814260c47ace4a723
Araceli Valdovinos <valdovinosaraceli50@gmail.com>
2021-07-02 13:47:32 -0500

    it have conditional statement in a function instead of promise method

--- src/main.js
+++ src/main.js
@@ -5,34 +5,40 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import ExchangeCurrency from './exchange-currency.js';
 
-
-// function getElement(response) {
-//   console.log(response);
-//   let EUR = response['conversion_rates']['EUR'];
-//   console.log(5*EUR)
-//   // if (response['conversion_rates']) {
-//   //   $()
-//   // }
-
-// }
 $('button#conversionBtn').click(function(){
+
+function getElement(response,) {
   let usDollars = parseInt($('input#userInput').val());
-  // console.log(usDollars);
+if ($('select#countries').val() === "EUR") {
+        $('#output').text((usDollars * `${response['conversion_rates']['EUR']}`).toFixed(2));
+      }else if ($('select#countries').val() === "JPY"){
+        $('#output').text((usDollars * `${response['conversion_rates']['JPY']}`).toFixed(2));
+      }else if ($('select#countries').val() === "GBP"){
+        $('#output').text((usDollars * `${response['conversion_rates']['GBP']}`).toFixed(2));
+      }else if ($('select#countries').val() === "AUD"){
+        $('#output').text((usDollars * `${response['conversion_rates']['AUD']}`).toFixed(2));
+      }else {
+        $('#output').text((usDollars * `${response['conversion_rates']['CAD']}`).toFixed(2));
+      }
+
+}
+// $('button#conversionBtn').click(function(){
+//   let usDollars = parseInt($('input#userInput').val());
   ExchangeCurrency.getCurrency()
     .then(function(response){
-      if ($('select#countries').val() === "EUR") {
-        $('#output').text(usDollars * `${response['conversion_rates']['EUR']}`);
-      }else if ($('select#countries').val() === "JPY"){
-        $('#output').text(usDollars * `${response['conversion_rates']['JPY']}`);
-      }else if ($('select#countries').val() === "GBP"){
-        $('#output').text(usDollars * `${response['conversion_rates']['GBP']}`);
-      }else if ($('select#countries').val() === "AUD"){
-        $('#output').text(usDollars * `${response['conversion_rates']['AUD']}`);
-      }else {
-        $('#output').text(usDollars * `${response['conversion_rates']['CAD']}`);
-      }
+      // if ($('select#countries').val() === "EUR") {
+      //   $('#output').text((usDollars * `${response['conversion_rates']['EUR']}`).toFixed(2));
+      // }else if ($('select#countries').val() === "JPY"){
+      //   $('#output').text((usDollars * `${response['conversion_rates']['JPY']}`).toFixed(2));
+      // }else if ($('select#countries').val() === "GBP"){
+      //   $('#output').text((usDollars * `${response['conversion_rates']['GBP']}`).toFixed(2));
+      // }else if ($('select#countries').val() === "AUD"){
+      //   $('#output').text((usDollars * `${response['conversion_rates']['AUD']}`).toFixed(2));
+      // }else {
+      //   $('#output').text((usDollars * `${response['conversion_rates']['CAD']}`).toFixed(2));
+      // }
       
-      // getElement(response);
+      getElement(response);
     });
 });
 
