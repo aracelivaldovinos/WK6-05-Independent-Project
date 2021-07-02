e418c69a1e2a847022ed815eb8f1a64eedfcec7f
Araceli Valdovinos <valdovinosaraceli50@gmail.com>
2021-07-02 14:11:50 -0500

    WOP: displaying errors

--- src/exchange-currency.js
+++ src/exchange-currency.js
@@ -2,15 +2,12 @@ export default class ExchangeCurrency {
   static getCurrency() {
     return (fetch (`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`))
       .then(function(response){
-        console.log(response);
-        if (!response.ok){
+        if (response.ok){
           throw Error(response.statusText);
         }
         return response.json();
-      })
-      .catch(function(error){
-        return error;
       });
+      
   }
   
 }
--- src/main.js
+++ src/main.js
@@ -7,38 +7,29 @@ import ExchangeCurrency from './exchange-currency.js';
 
 $('button#conversionBtn').click(function(){
 
-function getElement(response,) {
-  let usDollars = parseInt($('input#userInput').val());
-if ($('select#countries').val() === "EUR") {
-        $('#output').text((usDollars * `${response['conversion_rates']['EUR']}`).toFixed(2));
-      }else if ($('select#countries').val() === "JPY"){
-        $('#output').text((usDollars * `${response['conversion_rates']['JPY']}`).toFixed(2));
-      }else if ($('select#countries').val() === "GBP"){
-        $('#output').text((usDollars * `${response['conversion_rates']['GBP']}`).toFixed(2));
-      }else if ($('select#countries').val() === "AUD"){
-        $('#output').text((usDollars * `${response['conversion_rates']['AUD']}`).toFixed(2));
-      }else {
-        $('#output').text((usDollars * `${response['conversion_rates']['CAD']}`).toFixed(2));
-      }
-
-}
+  function getElement(response,) {
+    let usDollars = parseInt($('input#userInput').val());
+    if ($('select#countries').val() === "EUR") {
+      $('#output').text((usDollars * `${response['conversion_rates']['EUR']}`).toFixed(2));
+    }else if ($('select#countries').val() === "JPY"){
+      $('#output').text((usDollars * `${response['conversion_rates']['JPY']}`).toFixed(2));
+    }else if ($('select#countries').val() === "GBP"){
+      $('#output').text((usDollars * `${response['conversion_rates']['GBP']}`).toFixed(2));
+    }else if ($('select#countries').val() === "AUD"){
+      $('#output').text((usDollars * `${response['conversion_rates']['AUD']}`).toFixed(2));
+    }else {
+      $('#output').text((usDollars * `${response['conversion_rates']['CAD']}`).toFixed(2));
+    }
+  }
 // $('button#conversionBtn').click(function(){
 //   let usDollars = parseInt($('input#userInput').val());
   ExchangeCurrency.getCurrency()
-    .then(function(response){
-      // if ($('select#countries').val() === "EUR") {
-      //   $('#output').text((usDollars * `${response['conversion_rates']['EUR']}`).toFixed(2));
-      // }else if ($('select#countries').val() === "JPY"){
-      //   $('#output').text((usDollars * `${response['conversion_rates']['JPY']}`).toFixed(2));
-      // }else if ($('select#countries').val() === "GBP"){
-      //   $('#output').text((usDollars * `${response['conversion_rates']['GBP']}`).toFixed(2));
-      // }else if ($('select#countries').val() === "AUD"){
-      //   $('#output').text((usDollars * `${response['conversion_rates']['AUD']}`).toFixed(2));
-      // }else {
-      //   $('#output').text((usDollars * `${response['conversion_rates']['CAD']}`).toFixed(2));
-      // }
-      
+    .then(function(response){ 
       getElement(response);
+    })
+    .catch(function(error){
+      $('#output').text(`${response.message}`);   
+      console.log(error);
     });
 });
 
