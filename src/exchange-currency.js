e3f1cfcb59c055c2394c322d367ca77ff764fc2e
Araceli Valdovinos <valdovinosaraceli50@gmail.com>
2021-12-20 11:51:47 -0600

    add new api call

--- src/exchange-currency.js
+++ src/exchange-currency.js
@@ -1,11 +1,25 @@
-export default class ExchangeCurrency {
-  static getCurrency() {
-    return (fetch (`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`))
-      .then(function(response){
-        if (!response.ok){
-          throw Error(response.status);
-        }
-        return response.json();
-      });  
-  } 
+export function getCurrency() {
+  return (fetch (`https://v6.exchangerate-api.com/v6/1243dfd835c0236cbdb1a459/latest/USD`))
+    .then(function(response){
+      if (!response.ok){
+        throw Error(response.status);
+      }
+      return response.json();
+    });  
+} 
+
+export function getCountries() {
+  return (fetch (`https://restcountries.com/v3.1/all`))
+    .then(function(response){
+      if (!response.ok){
+        throw Error(response.status);
+      }
+      return response.json();
+    });  
 }
+
+
+
+
+
+
--- src/main.js
+++ src/main.js
@@ -1,32 +1,93 @@
-
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import ExchangeCurrency from './exchange-currency.js';
+import  {getCurrency, getCountries} from './exchange-currency.js';
 
-$('button#conversionBtn').click(function(){
+getCountries()
+  .then(function(response){
+    console.log(response);
+    response.map((item)=>{
+      console.log(item.name.common, item.cca3, item.flag);
+      $("#countries1").append(`<option value=${item.name.common}>${item.name.common} ${item.cca3} ${item.flag}</option>`);
+      $("#countries2").append(`<option value=${item.name.common}>${item.name.common} ${item.cca3} ${item.flag}</option>`);
+    });
+  })
+  .catch(function(error){   
+    console.log(error.message);
+  });
 
- 
-  ExchangeCurrency.getCurrency()
-    .then(function(response){ 
-      let currency = $('select#countries').val();
-      let usDollars = Math.round(parseInt($('input#userInput').val()));
-      const key = response['conversion_rates'];
-      if (currency in key){
-        let newCurrency = (usDollars*(key[currency]));
-        $('#output').text((newCurrency.toFixed(2)));
-      }
-      else {
-        $('#output').text("Currency does not exist!");
-      }
+getCurrency()
+  .then(function(response){
+    console.log(response);
+  })
+  .catch(function(error){   
+    console.log(error.message);
+  }); 
+  
+
+
+$('button').click(function(){
+  event.preventDefault();
+  // getCurrency()
+  //   .then(function(response){
+  //     console.log(response);
+  //   })
+  //   .catch(function(error){   
+  //     console.log(error.message);
+  //   }); 
+
+    
+  // getCountries()
+  //   .then(function(response){
+  //     console.log(response);
+  //     response.map((item)=>{
+  //       $("#countries").append(`<option value=${item.name.common}>${item.name.common}</option>`)
+  //       console.log(item.name.common);
+  //     });
+  //   })
+  //   .catch(function(error){   
+  //     console.log(error.message);
+  //   }); 
+  
+
+
+
+
+  
+  // getCountries();
+    // .then(function(response){
+    //   console.log(response);
+    // })
+    // .catch(function(error){
+    //   $('#output').text(`There was an error: ${error.message}`);   
+    //   console.log(error.message);
+    // }); 
+
+  //   getCurrency()
+  //     .then(function(response){ 
+  //       console.log(response);
+  //       let currency = $('select#countries').val();
+  //       let usDollars = Math.round(parseInt($('input#userInput').val()));
+  //       const key = response['conversion_rates'];
+  //       if (currency in key){
+  //         let newCurrency = (usDollars*(key[currency]));
+  //         $('#output').text((newCurrency.toFixed(2)));
+  //       }
+  //       else {
+  //         $('#output').text("Currency does not exist!");
+  //       }
               
-    })
-    .catch(function(error){
-      $('#output').text(`There was an error: ${error.message}`);   
-      console.log(error.message);
-    });    
-      
+  //     })
+  //     .catch(function(error){   
+  //       console.log(error.message);
+  //     }); 
+    
+  // async function makeApiCall() {
+  //   const response = await getCountries();
+  //   console.log(response); 
+  // }
+
+  // makeApiCall()
+        
 });
-
-
